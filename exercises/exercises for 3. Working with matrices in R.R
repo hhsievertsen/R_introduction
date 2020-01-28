dfc9325ab3c4c4fb3a14bc623e3f2ca0f16212a8
Hans Sievertsen <hs17922@bristol.ac.uk>
2020-01-28 15:39:07 +0000

    updated slides and exercises

---
+++ exercises/exercises for 3. Working with matrices in R.R
@@ -0,0 +1,32 @@
+# exercises for "Introduction to R" part "3. Working with matrices in R".
+# by Hans Henrik Sievertsen (h.h.sievertsen@bristol.ac.uk), Jan 28, 2020.
+# Objective: getting used to working with matrices in R.
+
+
+
+# Exercise 1: Create a matrix with 3 rows and 3 columns, where each cell value is a draw from
+#             a random normal distribution. Save as object A and print A in the console. 
+
+# Exercise 2: Create a matrix with 3 rows and 3 columns, where each cell value is a draw from
+#             a random normal distribution. Save the matrix as object B and multiply A with B 
+#             (using matrix multiplication, not element wise multiplication). 
+#             Save the resulting matrix as C and print C in the console.
+
+# Exercise 3: Compute the determinant of C.
+#             Is C  singular?
+
+# Exercise 4: Compute the inverse of C and verify that the inverse of C times C
+#             gives the identity matrix.
+
+# Exercise 5: load the dataset "example_data2.csv" and estimate the following model:
+#             test_year_2=alpha0+alpha1*parental_schooling+alpha2*parental_lincome+e
+#             using OLS with the ls() function. Print the results in the console.
+
+
+# Exercise 6: Estimate the same model as in Exercise 5, but this time you do it manually using 
+#             matrix algebra. Compare the results to the results from exercise 5.
+
+
+# Exercise 7: Compute the residuals from the regression and create a q-q plot. 
+
+
